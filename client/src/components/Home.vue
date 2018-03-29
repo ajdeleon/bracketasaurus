4ef3c96fa97cb8fd102c85c414906130fa220585
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-29 00:27:27 -0400

    add home component

---
+++ client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'You are now logged in!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
