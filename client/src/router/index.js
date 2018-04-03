cacd1fa9a44bbbd7a17933c7fa853c76acedb957
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-03 01:04:57 -0400

    set up routes in vue-router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '../components/Login'
 import Home from '../components/Home'
+import Current from '../components/Current'
+import Closed from '../components/Closed'
 
 Vue.use(Router)
 
@@ -13,9 +16,24 @@ export default new Router({
       component: Hello
     },
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/current',
+      name: 'Current',
+      component: Current
+    },
+    {
+      path: '/closed',
+      name: 'Closed',
+      component: Closed
     }
   ]
 })
