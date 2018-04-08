4c1ff977959ac5e47152e94096aab0430fef9102
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-08 15:07:20 -0400

    try oauth in index

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const passport = require('passport')
 const bodyParser = require('body-parser')
 const keys = require('./config/keys')
 const Sequelize = require('sequelize')
-require('./services/passport')
+//require('./services/passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const sequelize = new Sequelize(keys.dbname, keys.dbuser, keys.dbpassword, {
   dialect: 'mysql',
@@ -17,13 +18,27 @@ const User = sequelize.define('user', {
   googleId: Sequelize.TEXT
 })
 
-sequelize.sync()
-.then(() => {
-  User.create({
-    googleId: 'fjkldffsasdfjklghjkghjdhfgj'
-  })
-})
+// sequelize.sync()
+// .then(() => {
+//   User.create({
+//     googleId: 'fjkldffsasdfjklghjkghjdhfgj'
+//   })
+// })
+
 const app = express()
+// require('./routes/authRoutes')(app)
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+}, (accessToken) => {
+  console.log(accessToken)
+}
+))
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 // sequelize
 // .authenticate()
