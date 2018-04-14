8ca4d993ab47ef2950fbc5f3bc8b70bac9a7bff2
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-14 19:27:37 -0400

    update for proxy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 
 module.exports = app => {
   app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
-  
+
   app.get('/auth/google/callback', passport.authenticate('google', {
     successRedirect: '/api/current_user'
   }))
