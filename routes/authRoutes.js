9595b69c26695986373d2ea874c7a12d0b203cb1
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-14 13:05:54 -0400

    refactor for clarity and add logged in success route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,19 @@
 const passport = require('passport')
 
 module.exports = app => {
-  app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email']}))
+  app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+  
+  app.get('/auth/google/callback', passport.authenticate('google', {
+    successRedirect: '/api/current_user'
+  }))
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google'),
-    (req, res) => {
-      res.redirect('/home')
-    }
-  )
+  app.get('/api/current_user', (req, res) => {
+    res.send('User id: ' + req.user.id + ' is now logged in!')
+  })
 
   app.get('/api/logout', (req, res) => {
     req.logout()
-    res.redirect('/')
+    res.send(req.user)
   })
+
 }
--- services/passport.js
+++ services/passport.js
@@ -1,32 +1,38 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const Sequelize = require('sequelize')
 const keys = require('../config/keys')
-
-const User = sequelize.define('user', {
-  id: Sequelize.TEXT
-})
+const sequelize = require('../models/db')
+const User = sequelize.import('../models/User')
 
 passport.serializeUser((user, done)=> {
   done(null, user.id)
 })
 
-passport.deserializeUser(googleID, done => {
- User.findById(id) 
+passport.deserializeUser((id, done) => {
+   User.findById(id) 
     .then(user => {
       done(null, user)
-    })
+     })
 })
 
-passport.use(
-    new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      console.log(profile.id)  
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+}, async (accessToken, refreshToken, profile, done) => {
+    console.log('access', accessToken)
+    console.log('refresh token', refreshToken)
+    console.log('profile', profile)
+
+    const existingUser = await User.findOne({ where: { googleId: profile.id } })
+
+    if (existingUser) {
+      return done(null, existingUser)
     }
-  )
+
+    const user = await User.create({ googleId: profile.id })
+    done(null, user)
+  })
 )
+
